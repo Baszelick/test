192fcdab0add2b51a1adc1fc34bee2e09e213571
Baszelick <kydpu88@gmail.com>
2025-08-03 13:52:46 +0300

    Cards users

--- js/main.js
+++ js/main.js
@@ -1,10 +1,35 @@
-async function fetchUsers() {
+async function fetchAndRenderUsers() {
     try {
-        const response = await fetch("https://jsonplaceholder.typicode.com/users");
-        const users = await response.json();
-        console.log(users)
-    } catch (error) {
-        console.error('Не удалось загрузить пользователей:',error);
+    const usersContainer = document.getElementById('users-container');
+    usersContainer.textContent = 'Загрузка...';
+    const response = await fetch('https://jsonplaceholder.typicode.com/users')
+    if (!response.ok) {
+        throw new Error('Ошибка при загрузке пользователей');
     }
+        const users = await response.json();
+    usersContainer.innerHTML = '';
+    users.forEach(user => {
+        const card = document.createElement('div');
+        const name = document.createElement('h2');
+        const email = document.createElement('p');
+        const city = document.createElement('p');
+
+        card.classList.add('user-card');
+
+        name.textContent = user.name;
+        email.textContent = `Email: ${user.email}`
+        city.textContent = `Город: ${user.address.city}`
+
+        card.appendChild(name);
+        card.appendChild(email);
+        card.appendChild(city);
+
+        usersContainer.appendChild(card);
+    })
+
+    } catch (error) {
+        document.getElementById('users-container').textContent = `Ошибка: ${error.message}`;
+    }
+
 }
-fetchUsers()
+fetchAndRenderUsers()
