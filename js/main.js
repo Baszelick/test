fbd3fe0498346cfd6c41569e8bf22930e600927b
Baszelick <kydpu88@gmail.com>
2025-08-03 12:45:33 +0300

    Practice array methods

--- js/main.js
+++ js/main.js
@@ -1,15 +1,15 @@
-const mainHeader = document.querySelector('h1')
-mainHeader.addEventListener('mouseover', () => {
-    mainHeader.style.color = 'red'
-})
-mainHeader.addEventListener('mouseout', () => {
-    mainHeader.style.color = 'blue'
-})
+const users = [
+    { id: 1, name: 'Иван', isAdmin: false },
+    { id: 2, name: 'Анна', isAdmin: true },
+    { id: 3, name: 'Петр', isAdmin: false },
+    { id: 4, name: 'Ольга', isAdmin: true },
+]
 
-const loginForm = document.getElementById('login-form')
-loginForm.addEventListener('submit', (e) => {
-    e.preventDefault()
-    console.log('Попытка отправки формы!')
-})
+const userNames = users.map(user => user.name)
+console.log(userNames)
 
-mainHeader.textContent = 'Мой первый проект на Git!'
+const adminUsers = users.filter(user => user.isAdmin)
+console.log(adminUsers)
+
+const userWithId3 = users.find(user => user.id === 3)
+console.log(userWithId3)
